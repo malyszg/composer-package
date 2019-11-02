bc2b3c0707d276f860b887e54495b54f8c9e4c1e
malyszg <malyszg@o2.pl>
2019-11-02 17:30:52 +0100

    my ftp package

---
+++ src/FTP/FtpTools.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gtesmer
+ * Date: 2019-11-02
+ * Time: 17:26
+ */
+
+namespace FTP;
+
+
+class FtpTools
+{
+    public function test()
+    {
+        return 'ok';
+    }
+}
